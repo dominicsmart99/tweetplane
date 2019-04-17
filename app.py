ab93e2392bb2a179250683f9403b2565eac5dc95
adminuser <dominicsmart99@gmail.com>
2019-04-16 20:59:05 -0400

    make it better

--- app.py
+++ app.py
@@ -1,8 +1,5 @@
 from flask import Flask, render_template
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///asg3.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['TEMPLATES_AUTO_RELOAD'] = False  # use false for production
 
 
 

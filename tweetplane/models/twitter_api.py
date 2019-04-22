7ad725c3814479583d3f6ac8ed80b88c3768f937
Jerrel <jdjoneslmao@gmail.com>
2019-04-22 17:48:39 -0400

    my commit

--- tweetplane/models/twitter_api.py
+++ tweetplane/models/twitter_api.py
@@ -1,6 +1,182 @@
+from tweepy import StreamListener
+from tweepy import OAuthHandler
+from tweepy import Stream 
+#from credentials import *
+from tweepy import API
+from tweepy import Cursor 
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from textblob import TextBlob
+import re
+
+
+ACCESS_TOKEN = "1118229516256776194-8F2psTgSzPoX2Yfw1T9PkHbu6jj7lr"
+ACCESS_TOKEN_SECRET = "AjfGgtmBXg2EIxaQNCBSUR1QJPZVGeuMm9nxmqlthFF8l"
+CONSUMER_KEY = "vrrAuvS5lcCDU8KWPJIELXNja"
+CONSUMER_SECRET = "eOJ0DquGEmxfFFhaB03ygk5f67khCbZ9WH5sjdO1gT3MTbUXwe"
+BLUDEVGROUP_ID = 1118229516256776194
+
+
+class TwitterAuthenticator():
+    def authenticate_twitter_app(self):
+        auth = OAuthHandler(CONSUMER_KEY,CONSUMER_SECRET) #authentication that uses consumer credentials
+        auth.set_access_token(ACCESS_TOKEN,ACCESS_TOKEN_SECRET)
+        #using consumer key + secret, set access tokens
+        return auth
+        
+        
+    
+class TwitterStreamer():
+    '''
+    class for processing live tweets
+    '''
+    def __init__(self):
+        self.twitterAuthenticator = TwitterAuthenticator()
+        
+        
+    def stream_tweets(self, fetched_filename,hashlist):
+        #handles authentication and connection to the twitter API
+        listener = TwitterListener() #creates a listener object, how do i deal with the tweets and how do i deal with the errors
+        auth = self.twitter_autenticator.authenticate_twitter_app() 
+        stream = Stream(auth,listener)
+        stream.filter(track=hashlist) #filter method from the stream class that takes a list of key terms
+    
+
+class TwitterListener(StreamListener):
+    '''
+    prints received tweets to standard out
+    '''
+    def __init__(self, fetched_filename):
+        self.fetched_filename = fetched_filename
+        
+    def on_data(self,data):
+        try:
+            print(data)
+            with open(self.fetched_filename, 'a') as tf:
+                tf.write(data)
+            return True
+        except BaseException as e:
+            print("Error on_data: " + str(e))
+    def on_error(self,status):
+        if status == 420:
+            #returning false on_data method in the event of a rate limit
+            return False
+        print(status)
+        
+class TweetAnalyzer():
+    def tweets_to_dataframe(self,tweets):
+        dF = pd.DataFrame(data = [tweet.text for tweet in tweets], columns = ['tweets']) #declarative statement where we provide the DataFrame function with a list of tweets
+        dF['handle'] = np.array([tweet.user.name for tweet in tweets])
+        dF['likes'] = np.array([tweet.favorite_count for tweet in tweets])
+        dF['retweets'] = np.array([tweet.retweet_count for tweet in tweets])
+        #dF['len'] = np.array([len(tweet.text) for tweet in tweets])
+        dF['date'] = np.array([tweet.created_at for tweet in tweets])
+        #dF['source'] = np.array([tweet.source for tweet in tweets])
+        return dF
+        #Creating a dataframe from a list of tweets
+    #analyzing content of tweets
+    def append_sentiment(self,df,tweet_analyzer):
+        df['sentiment'] = np.array([tweet_analyzer.analyze_sentiment(tweet) for tweet in df['tweets']])
+        return df
+    
+    def convert_df_to_dictionary(self,df):
+        dic = df.to_dict('records')
+        return dic
+    
+    def clean_tweet(self,tweet):
+        return ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)", " ", tweet).split())
+    def analyze_sentiment(self,tweet):
+        analysis = TextBlob(self.clean_tweet(tweet))
+        if analysis.sentiment.polarity > 0:
+            return 1
+        elif analysis.sentiment.polarity == 0:
+            return 0
+        else:
+            return -1
+    
+             
+class TwitterClient():
+    def __init__(self,twitter_user=None):
+        self.auth = TwitterAuthenticator().authenticate_twitter_app()
+        self.twitter_client =  API(self.auth)
+        self.twitter_user = twitter_user
+        self.user = self.twitter_client.get_user(BLUDEVGROUP_ID)
+
+    def get_user_timeline_tweets(self,num_tweets):
+        tweets = []
+        for tweet in Cursor(self.twitter_client.user_timeline, id=self.twitter_user).items(num_tweets):
+            tweets.append(tweet)
+        return tweets
+    
+    def get_friend_list(self,num_friends):
+        friendList = []
+        for friend in Cursor(self.twitter_client.friends).items(num_friends):
+            friendList.append(friend)
+        return friendList
+    def get_home_timeline_tweets(self,num_tweets):
+        homeList = []
+        for tweet in Cursor(self.twitter_client.home_timeline).items(num_tweets):
+            homeList.append(tweet)
+        return homeList
+    
+    def get_twitter_client_api(self):
+        return self.twitter_client
+    
+    def get_tweets_by_handle(self,id_list):
+        tweet_list = []
+        for twit_id in id_list:
+            for tweet in Cursor(self.twitter_client.user_timeline, id=twit_id).items(3):
+                tweet_list.append(tweet)
+        return tweet_list
+    
+    def get_user_to_dict(self):
+       
+        return {
+                "username": self.user.name,
+                "bio": self.user.description,
+                "followers_count": self.user.followers_count
+                }
+  
+    def post_tweets(self, post_content):
+        self.twitter_client.update_status(post_content)
+        
+    def delete_last_tweet(self,tweet):
+        for last_tweet in tweet:
+            self.twitter_client.destroy_status(last_tweet.id)
+    
+    
+    
+    
+def create_chart(dic):
+    pos = 0
+    neut = 0
+    neg = 0
+    for rec in dic:
+        if rec['sentiment'] == 1:
+            pos += 1
+        elif rec['sentiment'] == 0:
+            neut += 1
+        else:
+            neg += 1
+     
+    objects = ('Positive', 'Negative','Neutral')
+    y_pos = np.arange(len(objects))
+    senti = [pos,neg,neut]
+    
+    plt.bar(y_pos,senti,align='center', alpha=0.5)
+    plt.xticks(y_pos,objects)
+    plt.ylabel('NUMBER OF TWEETS')
+    plt.title('SENTIMENT')
+    
+    plt.show()
+
+
+    
+
+'''
 from tweepy import StreamListener, OAuthHandler, Stream
 from credentials import *
-
 class StdOutListener(StreamListener):
 
 	def on_data(self, data):
@@ -17,3 +193,4 @@ auth.set_access_token(accessToken, accessTokenSecret)
 stream = Stream(auth, listener)
 
 stream.filter(track=['hentai'])
+'''
--- tweetplane/routes.py
+++ tweetplane/routes.py
@@ -1,9 +1,15 @@
 from flask import render_template, redirect, url_for, flash, request
 from tweetplane import app, db, bcrypt
 from tweetplane.models.forms import SignUpForm, LoginForm
+from tweetplane.models.twitter_api import TwitterClient, TwitterAuthenticator, TwitterListener,TweetAnalyzer
 from tweetplane.models.user import User
 from flask_login import login_user, logout_user, current_user, login_required
 
+
+
+uname=''
+
+#tweet_analyzer = TwitterAnalyzer()
 @app.before_first_request
 def setup():
     db.Model.metadata.drop_all(bind=db.engine)
@@ -19,11 +25,12 @@ def base():
 	return redirect(url_for("home"))
 
 @app.route('/home')
-def home():
+def home(name=None):
 	return render_template('home.html')
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    #uname = ''
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
@@ -31,7 +38,11 @@ def login():
             login_user(user, remember=form.remember.data)
             next_page = request.args.get('next')
             flash(f'Welcome back {form.username.data}!', 'success')
-            return redirect(next_page) if next_page else redirect(url_for('home'))
+            global uname
+            uname = form.username.data
+            return home()
+            
+            #return redirect(next_page) if next_page else redirect(url_for('home'))
         else: flash('Login unsuccessful. Please check email and password.', 'danger')
     return render_template('login.html', form=form)
 
@@ -53,6 +64,36 @@ def signup():
         flash(f'Your account has been created.', 'success')
         return redirect(url_for('login'))
     return render_template('signup.html', form=form)
+
+@app.route('/tweetlist', methods=['GET','POST'])
+def getTweets():
+    twitter_client = TwitterClient()
+    twitter_analyzer = TweetAnalyzer()
+    select = request.form.get('news_selector')
+    #print(select)
+    l=[]
+    l.append(select)
+    tweets = twitter_client.get_tweets_by_handle(l)
+    df = twitter_analyzer.tweets_to_dataframe(tweets)
+    dic = twitter_analyzer.convert_df_to_dictionary(df)
+    
+    return render_template('home.html',records=dic)
+  
+@app.route('/posttweet', methods = ['GET','POST'])
+def postTweet():
+    twitter_client = TwitterClient()
+    twitter_analyzer = TweetAnalyzer()
+    text = request.form.get('news_selector')
+    print(text)
+
+    #twitter_client.post_tweets(str(text))
+    tweets = twitter_client.get_user_timeline_tweets(1)
+    df = twitter_analyzer.tweets_to_dataframe(tweets)
+    dic = twitter_analyzer.convert_df_to_dictionary(df)
+    return render_template('home.html',records=dic)
+        
+    
+    
 
 @app.route('/account')
 @login_required
